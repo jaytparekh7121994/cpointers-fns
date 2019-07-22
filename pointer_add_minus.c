ff0dfa07c0f324b252e71959e7a829ef42ee2a35
jaytparekh7121994 <53175164+jaytparekh7121994@users.noreply.github.com>
2019-07-22 17:46:45 +0530

    Add files via upload

---
+++ pointer_add_minus.c
@@ -0,0 +1,47 @@
+/*
+This function method approach is known as Call By reference.
+
+Function void update(int *a,int *b), which reads two integers as argument, and sets  with the sum of them, and  with the absolute difference of them.
+
+Input Format
+The input will contain two integers,  and , separated by a newline.
+
+Output Format
+You have to print the updated value of  and , on two different lines.
+
+Sample Input
+	4
+	5
+
+Sample Output
+	9
+	1
+
+*/
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int *temp;
+    *temp=*a;
+    *a=((*a)+(*b));
+
+    if((*temp)-(*b) >0){
+        *b=(*temp)-(*b);
+    }
+    
+    else{
+        *b=(*b)-(*temp);
+    }
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    printf("Enter any two values a and b:");
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+    return 0;
+}
